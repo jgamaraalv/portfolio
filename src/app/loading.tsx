7755a3ebff93953662ce9e7105095da9a30fc827
João Amaral <guilive13@gmail.com>
2023-04-05 11:28:20 -0300

    loader adjust

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import styles from "./home.module.css";
 export default function Loading() {
   return (
     <section>
-      <div className={styles.content}>
+      <div className={`${styles.content} content-height`}>
         <div className="loader"></div>
       </div>
     </section>
